07bb7b8df978880193e1a19a774e5668a61f9481
Kevlar™ <109936702+KellyV2@users.noreply.github.com>
2022-08-17 02:19:27 +0000

    Add files via upload

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -1,3 +1,7 @@
 void copy_integers(int old_array[],int new_array[],int length) {
-
+    for (int i = 0; i < length; i++) {
+        for (int j = 0; j < 2; j++) {
+            new_array[i][j] = old_array[i][j];
+        }
+    }
 }
--- main-1-3.cpp
+++ main-1-3.cpp
@@ -5,9 +5,9 @@ using namespace std;
 extern void copy_integers(int old_array[],int new_array[],int length);
 
 int main() {
-    int old_array;
-    int new_array;
-    int length;
+    int old_array[3][2] = {{1, 1}, {1, 1}, {1, 1}};
+    int new_array[3][2];
+    int length = 3;
 
     copy_integers(old_array, new_array, length);
 
