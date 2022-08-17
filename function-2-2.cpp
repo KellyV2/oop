145c28e69fe03edb3684e0f3f9c4dc6c5a1ef410
Kevlar™ <109936702+KellyV2@users.noreply.github.com>
2022-08-17 05:02:38 +0000

    Add files via upload

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -1,3 +1,18 @@
 int max_sub_sum(int *nums,int length) {
-
+    int sum = 0, neg = 0;
+    if (length < 1) {
+        return 0;
+    }
+    for (int i = 0; i < length; i++) {
+        if (*(nums+i) < 0) {
+            neg++;
+        }
+    }
+    if (neg == length) {
+        return 0;
+    }
+    for (int j = 0; j < length; j++) {
+        
+    }
+    return sum;
 }
--- main-2-2.cpp
+++ main-2-2.cpp
@@ -1,5 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
 extern int max_sub_sum(int *nums,int length);
 
 int main() {
+    int nums[3] = {1, 1, 1};
+    int length = 3;
+
+    cout << max_sub_sum(nums, length) << endl;
+
     return 0;
 }
