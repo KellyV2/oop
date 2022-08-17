0207f9d7bc1a20c800a150f85414f8587e4aa584
Kevlar™ <109936702+KellyV2@users.noreply.github.com>
2022-08-17 03:53:08 +0000

    Add files via upload

--- main-1-3.cpp
+++ main-1-3.cpp
@@ -5,11 +5,11 @@ using namespace std;
 extern void copy_integers(int old_array[],int new_array[],int length);
 
 int main() {
-    int old_array[3][2] = {{1, 1}, {1, 1}, {1, 1}};
-    int new_array[3][2];
+    int old_array[3] = {1, 1, 1};
+    int new_array[3];
     int length = 3;
 
-    copy_integers(*old_array, *new_array, length);
+    copy_integers(old_array, new_array, length);
 
     return 0;
 }
