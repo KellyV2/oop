bb062b2fafc21cea170c0a9d6ce90e0283a698de
Kevlar™ <109936702+KellyV2@users.noreply.github.com>
2022-08-17 04:02:35 +0000

    Add files via upload

--- main-1-4.cpp
+++ main-1-4.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 
-using namespace std;
-
 extern void copy_doubles(double *old_array,double *new_array,int length);
 
 int main() {
+    double old_array[3] = {1, 1, 1};
+    double new_array[3];
+    int length = 3;
+
+    copy_doubles(old_array, new_array, length);
+
     return 0;
 }
